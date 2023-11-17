9a1dfa8db82e7152823839ce196e016b045ba6f6
Max <frolovmaxim86@gmail.com>
2023-11-17 20:34:37 +0300

    edit PW3/main.py

--- PW3/main.py
+++ PW3/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from image_to_text.router import router as router_image_to_text
+from question_answerer.router import router as router_question_answerer
 
 app = FastAPI(
     title="Common api"
 )
 
 app.include_router(router_image_to_text)
+app.include_router(router_question_answerer)
