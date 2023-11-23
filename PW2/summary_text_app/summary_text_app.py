b974b28e03f1a0707a068e8c4e617998e4930e49
Anton Vlasov <nocsland@gmail.com>
2023-11-23 16:42:30 +0400

    update app summary_text_app

--- PW2/summary_text_app/summary_text_app.py
+++ PW2/summary_text_app/summary_text_app.py
@@ -1,39 +1,35 @@
-import re
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import streamlit as st
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 
 model_name = "csebuetnlp/mT5_multilingual_XLSum"
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, legacy=False)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-st.title("Генерация краткого содержания текста")
-
-article_text = st.text_input("Введите текст для анализа")
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, legacy=False)
 
 
-WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
+# Декоратор @st.cache говорит Streamlit, что модель нужно загрузить только один раз, чтобы избежать утечек памяти
+@st.cache_resource
+# загружает модель
+def load_model():
+    return pipeline("summarization", model=model, tokenizer=tokenizer)
 
-input_ids = tokenizer(
-    [WHITESPACE_HANDLER(article_text)],
-    return_tensors="pt",
-    padding="max_length",
-    truncation=True,
-    max_length=512
-)["input_ids"]
 
-output_ids = model.generate(
-    input_ids=input_ids,
-    max_length=84,
-    no_repeat_ngram_size=2,
-    num_beams=4
-)[0]
+# Загружаем предварительно обученную модель
+summary_text = load_model()
 
-summary = tokenizer.decode(
-    output_ids,
-    skip_special_tokens=True,
-    clean_up_tokenization_spaces=False
-)
-if article_text:
-    if article_text:
-        st.subheader("Краткое содержание:")
-        st.subheader(summary, divider='rainbow')
+# Выводим заголовок страницы
+st.title("Генерация краткого содержания")
+
+# Получаем текст для анализа
+text = st.text_input("Введите текст для анализа")
+
+# Создаем кнопку
+button = st.button('Генерировать')
+
+# Формируем результат
+result = summary_text(text)[0]['summary_text']
+
+# Выводим результат по нажатию кнопки
+if button:
+    st.subheader("Краткое содержание:")
+    st.write(result)
