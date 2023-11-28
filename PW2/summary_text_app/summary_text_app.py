4492ef6437ccce837e872800a953d2924bfa1e5f
Anton Vlasov <nocsland@gmail.com>
2023-11-28 23:44:08 +0400

    added exception "file is empty"

--- PW2/summary_text_app/summary_text_app.py
+++ PW2/summary_text_app/summary_text_app.py
@@ -20,13 +20,27 @@ summary_text = load_model()
 # Выводим заголовок страницы
 st.title("Генерация краткого содержания")
 
-# Получаем текст для анализа
-text = st.text_area("Введите текст для анализа")
-
+# Выбираем источник данных
+source_button = st.radio(
+    "Выберете источник данных",
+    ["Ввод текста", "Загрузка файла"],
+    captions=["Вставить текст из буфера, или ввести с клавиатуры", "Загрузить текст из файла формата .txt"])
+if source_button == "Ввод текста":
+    # Источник буфер или клавиатура
+    text = st.text_area("Введите текст для анализа")
+elif source_button == "Загрузка файла":
+    # Источник файл .txt
+    uploaded_files = st.file_uploader("Выберете .txt файлы", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        text = bytes_data.decode()
 # Создаем кнопку
 button = st.button('Генерировать')
 
 # Выводим результат по нажатию кнопки
 if button:
-    st.subheader("Краткое содержание:")
-    st.write(summary_text(text)[0]['summary_text'])
+    try:
+        st.subheader("Краткое содержание:")
+        st.write(summary_text(text)[0]['summary_text'])
+    except NameError: st.write("File is empty")
+
