a7e0160e9b1382afe11b8e644bddfcce0ee8bd06
Anton Vlasov <nocsland@gmail.com>
2023-12-03 21:13:47 +0400

    update summary_text_app.py, added config.toml

--- PW2/summary_text_app/summary_text_app.py
+++ PW2/summary_text_app/summary_text_app.py
@@ -7,40 +7,31 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name, legasy=False)
 
 
-# @st.cache модель нужно загрузить только один раз
 @st.cache_resource
-# загружает модель
 def load_model():
     return pipeline("summarization", model=model, tokenizer=tokenizer)
 
 
-# Загружаем предварительно обученную модель
 summary_text = load_model()
-
-# Выводим заголовок страницы
-st.title("Генерация краткого содержания")
+st.title("Создание краткого резюме")
 st.write("Вы можете использовать текст на любом из 43 языков")
 
-# Выбираем источник данных
 source_button = st.radio(
-    "\nВыберете источник данных",
+    "\nВыберите источник данных",
     ["Ввод текста", "Загрузка файла"],
     captions=["Вставить текст из буфера или ввести с клавиатуры", "Загрузить текст из файла формата TXT"])
 if source_button == "Ввод текста":
-    # Источник буфер или клавиатура
-    text = st.text_area("Введите текст для анализа")
+    text = st.text_area("Введите текст")
 elif source_button == "Загрузка файла":
-    # Источник файл TXT
-    uploaded_file = st.file_uploader("Выберете файл", type='txt', accept_multiple_files=False)
+    uploaded_file = st.file_uploader("Выберите файл", type='txt', accept_multiple_files=False)
     if uploaded_file is not None:
         text = uploaded_file.read().decode()
-# Создаем кнопку
-button = st.button('Генерировать')
-
-# Выводим результат по нажатию кнопки
+    else:
+        text = ""
+button = st.button('Создать')
 if button:
     try:
-        st.subheader("Краткое содержание:")
+        st.markdown("**Результат:**")
         st.write(summary_text(text, max_length=200, min_length=50)[0]['summary_text'])
-    except NameError:
-        st.write("Файл не загружен")
+    except Exception as e:
+        st.write(f"Ошибка: {e}")
