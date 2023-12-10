2276784d98b0c5b0a91350cd722ecda69eca4ced
Evgeniy Timofeev <evgenij.timofeev.95@list.ru>
2023-12-10 15:57:02 +0300

    Add

---
+++ PW3/text_sum/router.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from summary_text.model import load_model
+
+router = APIRouter(
+    prefix="/summary_text",
+    tags=["SummaryText"]
+)
+
+
+class Item(BaseModel):
+    text: str
+
+
+@router.get("/")
+async def base_page():
+    """
+    Возвращает приветственное сообщение
+    """
+    return {"message": "Welcome to Base Page"}
+
+
+@router.post("/")
+async def summary_text(
+        item: Item,
+        model=Depends(load_model),
+):
+    """
+    Генерирует краткое содержание введенного текста
+    """
+    try:
+        # генерация ответа
+        result = model(item.text)
+
+        return {
+            "Краткое содержание: ": result[0]['summary_text'],
+
+        }
+
+    except Exception as e:
+        return {"Error message": e.__str__()}
