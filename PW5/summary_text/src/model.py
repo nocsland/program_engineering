ed68ebbfb6412d6135984d050ee8912186a9ff49
Anton Vlasov <nocsland@gmail.com>
2023-12-10 16:20:06 +0400

    updated load_model

--- PW5/summary_text/src/model.py
+++ PW5/summary_text/src/model.py
@@ -1,10 +1,9 @@
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-model_name = "csebuetnlp/mT5_multilingual_XLSum"
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, legacy=False)
-
 
 def load_model():
+    model_name = "csebuetnlp/mT5_multilingual_XLSum"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, legacy=False)
     return pipeline("summarization", model=model, tokenizer=tokenizer)
