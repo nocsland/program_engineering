a6cdc4479fe0cf3d4a7f4f97455132d67eeffb51
italian <frolovmaxim86@gmail.com>
2024-03-16 14:55:43 +0300

    add spinner

--- PJ/summary_text/summary_text.py
+++ PJ/summary_text/summary_text.py
@@ -53,13 +53,14 @@ def main():
 
     if button:
         try:
-            # выводим результат
-            st.markdown("**Результат:**")
-            st.write(summary_text(
-                text,
-                max_length=200,
-                min_length=50,
-            )[0]["summary_text"])
+            with st.spinner('Ждите...'):
+                # выводим результат
+                st.markdown("**Результат:**")
+                st.write(summary_text(
+                    text,
+                    max_length=200,
+                    min_length=50,
+                )[0]["summary_text"])
 
         except Exception as e:
             # выводим возникающие ошибки
